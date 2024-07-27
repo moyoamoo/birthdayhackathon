734ab6e3c37f58437e4f8460e6a1c82fbe84c0e2
Your Name <manzi5000@hotmail.co.uk>
2024-07-27 10:39:26 +0100

    moved email to app

--- src/Email.jsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@react-email/components";
-
-const Email = () => {
-  return (
-    <Button href="https://example.com" style={{ color: "#61dafb" }}>
-      Click me
-    </Button>
-  );
-};
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,9 +1,14 @@
-import { Button } from "@react-email/components";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
+import App from "./App.jsx";
+import "./css/index.css";
 
-const Email = () => {
-  return (
-    <Button href="https://example.com" style={{ color: "#61dafb" }}>
-      Click me
-    </Button>
-  );
-};
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
